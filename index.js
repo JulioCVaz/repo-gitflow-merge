12252193936c8a661dca6f85517e881b7d867efa
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-04-25 17:11:06 -0300

    release/1.0.0 (#7)
    * chore/squash 1 (#1)
    
    * feat: squash 1 console
    
    * feat: squash 1 console 2
    
    * feat: squash 1 console 3
    
    * chore/squash 2 (#2)
    
    * feat: squash 2 console 1
    
    * feat: squash 2 console 2
    
    * feat: merge 1 console 1
    
    * feat: merge 1 console 2
    
    * feat: merge 2 console 1
    
    * feat: merge 2 console 2
    
    * feat: merge 3 console 1
    
    * feat: merge 3 console 2

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 console.log("first commit"); // push - merge
 console.log("codepush");
+console.log("squash 1");
+console.log("squash 2");
+console.log("squash 3");
+console.log("squash 2 1");
+console.log("squash 2 2");
+console.log("merge 1");
+console.log("merge 2");
+console.log("merge 2 1");
+console.log("merge 2 2");
+console.log("merge 3 1");
+console.log("merge 3 2");
