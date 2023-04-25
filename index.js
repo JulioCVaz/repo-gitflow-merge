229617de594b37669f32711d7620d2480cf41d15
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-04-25 16:42:48 -0300

    feat: merge 3 console 1

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ console.log("merge 1");
 console.log("merge 2");
 console.log("merge 2 1");
 console.log("merge 2 2");
+<<<<<<< HEAD
 console.log("codepush 3");
 console.log("merge 3 1");
 console.log("merge 3 2");
+=======
+console.log("merge 3 1");
+>>>>>>> cbc2206 (feat: merge 3 console 1)
