de7c46526d9a375015f5b0a44ecd2bdb9a3aad41
Juliovaz <julio.oliveiravaz0@gmail.com>
2023-04-25 17:24:17 -0300

    feat: add codepush 2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ console.log("squash 2");
 console.log("squash 3");
 console.log("squash 2 1");
 console.log("squash 2 2");
+console.log("codepush 2");
 console.log("merge 1");
 console.log("merge 2");
 console.log("merge 2 1");
